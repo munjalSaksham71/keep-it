f8dc35ebdf84a861e4063c21fdbc4e0004216c2f
munjalSaksham71 <sakshammunjal4@gmail.com>
2022-03-29 13:03:41 +0530

    Add: View Notes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { useState } from 'react';
 import './App.css';
 import AddNote from './components/AddNote/AddNote';
 import Header from './components/Header/Header';
+import ViewNote from './components/ViewNote/ViewNote';
 
 function App() {
+  
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  console.log(notes);
   return (
     <div className="App">
       <Header />
-      <AddNote />
+      <AddNote onAdd={addNote} />
+      <ViewNote notes={notes} onDelete={deleteNote} />
     </div>
   );
 }
--- src/components/AddNote/AddNote.jsx
+++ src/components/AddNote/AddNote.jsx
@@ -1,13 +1,49 @@
-import './AddNote.css'
+import { useState } from "react";
+import "./AddNote.css";
 
-const AddNote = () => {
-    return (
-        <div className="add_note">
-            <input type="text" placeholder="Title" className="title" />
-            <input type="text" placeholder="Add a Note..." className="note" />
-            <button className="btn btn-primary mt-3">Add</button>
-        </div>
-    )
-}
+const AddNote = (props) => {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
 
-export default AddNote
+  function handleChange(e) {
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [e.target.name]: e.target.value,
+      };
+    });
+  }
+
+  const submitNote = (e) => {
+    e.preventDefault();
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+  };
+
+  return (
+    <form className="add_note">
+      <input
+        name="title"
+        onChange={handleChange}
+        value={note.title}
+        placeholder="Title"
+        className="title"
+      />
+      <input
+        name="content"
+        onChange={handleChange}
+        value={note.content}
+        placeholder="Take a note..."
+        className="note"
+      />
+      <button onClick={submitNote} className="btn btn-primary">Add</button>
+    </form>
+  );
+};
+
+export default AddNote;
